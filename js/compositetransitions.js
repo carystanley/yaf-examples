f56da46bfb5996f232b534e4cfb2f0a2d3447f58
Cary Stanley <carystanley@example.com>
2013-02-25 15:44:54 -0800

    Factored out Composite Transitions as own MixIn

---
+++ js/compositetransitions.js
@@ -0,0 +1,63 @@
+YUI.add('compositetransitions', function (Y, NAME) {
+
+Y.CompositeTransitions = function() {};
+Y.CompositeTransitions.prototype = {
+
+  transitionRegionView: function(region, view, callback) {
+    var self = this,
+        container = this.get('container'),
+        prepend = false,
+        transitioning = Y.App.CLASS_NAMES.transitioning,
+        oldView,
+        newView = (Y.Lang.isString(view)) ? this.getView(view) : view,
+        regionContainer = container.one('[data-region="'+region+'"]'),
+        transitions,
+        fxConfig,
+        fx;
+ 
+    oldView = this.getRegionView(region);
+
+    if (oldView == newView) return callback && callback.call(self, newView);
+
+    this.setRegionView(region, view);
+
+    regionContainer[prepend ? 'prepend' : 'append'](newView.get('container'));
+    newView.addTarget(this);
+
+    container.addClass(transitioning);
+    transitions = new Y.Parallel({context: this});
+    fxConfig    = {
+        crossView: !!oldView && !!newView,
+        prepended: prepend
+    };
+    fx = {
+      viewIn: 'app:slideLeft', 
+      viewOut: 'app:slideLeft' 
+    };
+
+    if (newView && fx.viewIn) {
+      newView.get('container')
+        .transition(fx.viewIn, fxConfig, transitions.add());
+    }
+
+    if (oldView && fx.viewOut) {
+      oldView.get('container')
+        .transition(fx.viewOut, fxConfig, transitions.add());
+    }
+
+    function complete() {
+      oldView.remove();
+      oldView.removeTarget(self);
+      container.removeClass(transitioning);
+      callback && callback.call(self, newView);
+    }
+
+    transitions.done(complete);
+  }
+
+};
+
+}, '0.0.2', {
+    requires : []
+});
+
