2dd4eac0f7cb14982566c02d50a384d74f268ef7
Cary Stanley <carystanley@example.com>
2013-02-25 14:29:32 -0800

    Added Rough Transitions

--- js/compositeview.js
+++ js/compositeview.js
@@ -60,6 +60,55 @@ Y.CompositeView.prototype = {
   setView: function(viewid, view) {
     if (!this.views) this.views = {};
     this.views[viewid] = view;
+  },
+
+  transitionRegionView: function(region, view, callback) {
+    var self = this,
+        container = this.get('container'),
+        prepend = false,
+        transitioning = Y.App.CLASS_NAMES.transitioning,
+        oldView,
+        newView = (Y.Lang.isString(view)) ? this.getView(view) : view,
+        regionContainer = container.one('[data-region="'+region+'"]'),
+        transitions,
+        fxConfig,
+        fx;
+ 
+    oldView = this.getRegionView(region);
+    this.setRegionView(region, view);
+
+    regionContainer[prepend ? 'prepend' : 'append'](newView.get('container'));
+    newView.addTarget(this);
+
+    container.addClass(transitioning);
+    transitions = new Y.Parallel({context: this});
+    fxConfig    = {
+        crossView: !!oldView && !!newView,
+        prepended: prepend
+    };
+    fx = {
+      viewIn: 'app:slideLeft', 
+      viewOut: 'app:slideLeft' 
+    };
+
+    if (newView && fx.viewIn) {
+      newView.get('container')
+        .transition(fx.viewIn, fxConfig, transitions.add());
+    }
+
+    if (oldView && fx.viewOut) {
+      oldView.get('container')
+        .transition(fx.viewOut, fxConfig, transitions.add());
+    }
+
+    function complete() {
+      oldView.remove();
+      oldView.removeTarget(self);
+      container.removeClass(transitioning);
+      callback && callback.call(self, newView);
+    }
+
+    transitions.done(complete);
   }
 
 };
